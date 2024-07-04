df9666d0bc2aad31b4c958373c913563b4e251f5
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2024-07-04 13:50:14 +0200

    Obsidian nvim plugin addition.

--- init.lua
+++ init.lua
@@ -257,7 +257,8 @@ require('lazy').setup({
       },
     },
   },
-  return {
+
+  {
     "epwalsh/obsidian.nvim",
     version = "*",  -- recommended, use latest release instead of latest commit
     lazy = true,
